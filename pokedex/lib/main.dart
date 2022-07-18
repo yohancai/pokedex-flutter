b17a5325d740c53ea66e1ff3107e35220e103df2
Yohan Cai <yohan.cai@ffuf.de>
2022-07-18 19:13:57 +0800

    Activity 2

--- pokedex/lib/main.dart
+++ pokedex/lib/main.dart
@@ -9,6 +9,8 @@ void main() {
 
 List<Pokemon> pokemon_list = [];
 List<String> pokemonImage_list = [];
+ScrollController _scrollController = ScrollController();
+int offset = 20;
 
 class MyApp extends StatefulWidget {
   @override
@@ -21,11 +23,10 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    getPokemon();
+    getPokemon(offset);
 
-    Future.delayed(const Duration(milliseconds: 5000), (){
+    Future.delayed(const Duration(milliseconds: 5000), () {
       setState(() {
-        isLoading = false;
         print("setstate");
       });
     });
@@ -36,57 +37,237 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(appBar: AppBar(
+      home: Scaffold(appBar: AppBar(
         title: Text("Pokedex"),
-        ),
-    body: isLoading
-        ? const Center(
-      child: CircularProgressIndicator(),
-    ): GridView.count(
-      crossAxisCount: 2,
-      children: List.generate(20, (index) {
-        return Center(
-          child: Column(
-            children: <Widget> [
+      ),
+          body: NotificationListener(
+            child: GridView.builder(
+              controller: _scrollController,
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  childAspectRatio: 1,
+                  crossAxisSpacing: 1,
+                  mainAxisSpacing: 1),
+              itemCount: pokemon_list.length,
+              itemBuilder: (context, index) {
+                return Container(
 
-              Text(
-                pokemon_list[index].name,
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                child: Image.network(pokemonImage_list[index]),
-              ),
-            ],
-          ),
-        );
-      }),
-    ),
-    )
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        pokemon_list[index].name,
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .headline5,
+                      ),
+                      if(pokemon_list[index].type == "fire") ... [
+                        Container(
+                          color: Colors.red,
+                          width: 100,
+                          height: 100,
+                          child: Image.network(pokemonImage_list[index]),
+                        ),
+                      ]
+                      else if(pokemon_list[index].type == "grass") ... [
+                        Container(
+                          color: Colors.lightGreen,
+                          width: 100,
+                          height: 100,
+                          child: Image.network(pokemonImage_list[index]),
+                        ),
+                      ]
+                      else if(pokemon_list[index].type == "water") ... [
+                          Container(
+                            color: Colors.blueAccent,
+                            width: 100,
+                            height: 100,
+                            child: Image.network(pokemonImage_list[index]),
+                          ),
+                        ]
+                      else if(pokemon_list[index].type == "normal") ... [
+                          Container(
+                            color: Colors.grey,
+                            width: 100,
+                            height: 100,
+                            child: Image.network(pokemonImage_list[index]),
+                          ),
+                        ]
+                      else if(pokemon_list[index].type == "flying") ... [
+                          Container(
+                            color: Colors.lightBlue,
+                            width: 100,
+                            height: 100,
+                            child: Image.network(pokemonImage_list[index]),
+                          ),
+                        ]
+                            else if(pokemon_list[index].type == "electric") ... [
+                                Container(
+                                  color: Colors.yellow,
+                                  width: 100,
+                                  height: 100,
+                                  child: Image.network(pokemonImage_list[index]),
+                                ),
+                              ]
+                              else if(pokemon_list[index].type == "ground") ... [
+                                  Container(
+                                    color: Colors.orangeAccent,
+                                    width: 100,
+                                    height: 100,
+                                    child: Image.network(pokemonImage_list[index]),
+                                  ),
+                                ]
+                                else if(pokemon_list[index].type == "fighting") ... [
+                                    Container(
+                                      color: Colors.deepOrange,
+                                      width: 100,
+                                      height: 100,
+                                      child: Image.network(pokemonImage_list[index]),
+                                    ),
+                                  ]
+                                  else if(pokemon_list[index].type == "psychic") ... [
+                                      Container(
+                                        color: Colors.purpleAccent,
+                                        width: 100,
+                                        height: 100,
+                                        child: Image.network(pokemonImage_list[index]),
+                                      ),
+                                    ]
+                                    else if(pokemon_list[index].type == "rock") ... [
+                                        Container(
+                                          color: Colors.brown,
+                                          width: 100,
+                                          height: 100,
+                                          child: Image.network(pokemonImage_list[index]),
+                                        ),
+                                      ]
+                                      else if(pokemon_list[index].type == "ice") ... [
+                                          Container(
+                                            color: Colors.blueGrey,
+                                            width: 100,
+                                            height: 100,
+                                            child: Image.network(pokemonImage_list[index]),
+                                          ),
+                                        ]
+                                        else if(pokemon_list[index].type == "ghost") ... [
+                                            Container(
+                                              color: Colors.deepPurple,
+                                              width: 100,
+                                              height: 100,
+                                              child: Image.network(pokemonImage_list[index]),
+                                            ),
+                                          ]
+                                          else if(pokemon_list[index].type == "dragon") ... [
+                                              Container(
+                                                color: Colors.deepPurpleAccent,
+                                                width: 100,
+                                                height: 100,
+                                                child: Image.network(pokemonImage_list[index]),
+                                              ),
+                                            ]
+                                            else if(pokemon_list[index].type == "dark") ... [
+                                                Container(
+                                                  color: Colors.black45,
+                                                  width: 100,
+                                                  height: 100,
+                                                  child: Image.network(pokemonImage_list[index]),
+                                                ),
+                                              ]
+                                              else if(pokemon_list[index].type == "steel") ... [
+                                                  Container(
+                                                    color: Colors.blueGrey,
+                                                    width: 100,
+                                                    height: 100,
+                                                    child: Image.network(pokemonImage_list[index]),
+                                                  ),
+                                                ]
+                                                else if(pokemon_list[index].type == "fairy") ... [
+                                                    Container(
+                                                      color: Colors.pink,
+                                                      width: 100,
+                                                      height: 100,
+                                                      child: Image.network(pokemonImage_list[index]),
+                                                    ),
+                                                  ]
+                      else ... [
+                        Container(
+                          color: Colors.brown,
+                          width: 100,
+                          height: 100,
+                          child: Image.network(pokemonImage_list[index]),
+                        ),
+                      ]
+
+                    ],
+                  ),
+                );
+              },
+            ),
+            onNotification: (end) {
+              if (end is ScrollEndNotification) {
+                offset = offset + 20;
+                addPokemon(offset);
+                Future.delayed(const Duration(milliseconds: 5000), () {
+                  setState(() {
+                    print("setstate");
+                  });
+                });
+              }
+              return true;
+            },
+          )
+      ),
     );
   }
-}
 
-void getPokemon() async {
-  var url;
-  for(int i = 1; i <= 20; i++){
-    pokemonImage_list.add("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$i.png");
-    url = Uri.parse("https://pokeapi.co/api/v2/pokemon/$i/");
-    var response = await http.get(
-        url
-    );
-    if(response.statusCode == 200){
-      String data = response.body;
-      final parsedJson = convert.jsonDecode(data);
-      final pokemon = Pokemon.fromJson(parsedJson);
-      print(pokemon.name);
-      pokemon_list.add(pokemon);
-      print("THIS QUERY = "+pokemon_list[i-1].name+" SUCCESS");
+  Future<List<Pokemon>> getPokemon(int offset) async {
+    var pokemonurl;
+    pokemonImage_list.clear();
+    pokemon_list.clear();
+    for (int i = 1; i <= offset; i++) {
+      pokemonImage_list.add(
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$i.png");
+      pokemonurl = Uri.parse("https://pokeapi.co/api/v2/pokemon/$i/");
+      var response = await http.get(
+          pokemonurl
+      );
+      if (response.statusCode == 200) {
+          String data = response.body;
+          final parsedJson = convert.jsonDecode(data);
+          final pokemon = Pokemon.fromJson(parsedJson);
+          final type = typeFromJson(data);
+          pokemon.type = type.types[0].type.name;
+          pokemon_list.add(pokemon);
+          print("Added " + pokemon.name);
+      }
+      else {
+        print("response status code is " + response.statusCode.toString());
+      }
     }
-    else{
-      print("response status code is " + response.statusCode.toString());
+    return pokemon_list;
+  }
+  Future<List<Pokemon>> addPokemon(int offset) async {
+    var url;
+    for (int i = offset; i <= offset+20; i++) {
+      pokemonImage_list.add(
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$i.png");
+      url = Uri.parse("https://pokeapi.co/api/v2/pokemon/$i/");
+      var response = await http.get(
+          url
+      );
+      if (response.statusCode == 200) {
+        String data = response.body;
+        final parsedJson = convert.jsonDecode(data);
+        final pokemon = Pokemon.fromJson(parsedJson);
+        final type = typeFromJson(data);
+        pokemon.type = type.types[0].type.name;
+        pokemon_list.add(pokemon);
+        print("Added " + pokemon.name);
+      }
+      else {
+        print("response status code is " + response.statusCode.toString());
+      }
     }
-
+    return pokemon_list;
   }
 }
--- pokedex/lib/models/Pokemon.dart
+++ pokedex/lib/models/Pokemon.dart
@@ -1,11 +1,75 @@
 
+import 'dart:convert';
+
 class Pokemon {
   Pokemon(
     this.name,
   );
   String name = "";
+  String type = "";
 
   Pokemon.fromJson(Map<String, dynamic> json) {
     name = json['name'];
   }
+
 }
+ArrType typeFromJson(String str) => ArrType.fromJson(json.decode(str));
+
+String welcomeToJson(ArrType data) => json.encode(data.toJson());
+
+class ArrType {
+  ArrType({
+    required this.types,
+  });
+
+  List<TypeElement> types;
+
+  factory ArrType.fromJson(Map<String, dynamic> json) => ArrType(
+    types: List<TypeElement>.from(json["types"].map((x) => TypeElement.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "types": List<dynamic>.from(types.map((x) => x.toJson())),
+  };
+}
+
+class TypeElement {
+  TypeElement({
+    required this.slot,
+    required this.type,
+  });
+
+  int slot;
+  Types type;
+
+  factory TypeElement.fromJson(Map<String, dynamic> json) => TypeElement(
+    slot: json["slot"],
+    type: Types.fromJson(json["type"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "slot": slot,
+    "type": type.toJson(),
+  };
+}
+
+class Types {
+  Types({
+    required this.name,
+    required this.url,
+  });
+
+  String name;
+  String url;
+
+  factory Types.fromJson(Map<String, dynamic> json) => Types(
+    name: json["name"],
+    url: json["url"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "url": url,
+  };
+}
+
